797d6fbc497def22849b847df30fb124c243e94f
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2022-02-25 18:35:35 -0500

    decode activos fijos

--- modelos/Activos_fijos.php
+++ modelos/Activos_fijos.php
@@ -1,30 +1,27 @@
-<?php 
+<?php
 //Incluímos inicialmente la conexión a la base de datos
 require "../config/Conexion.php";
 
-Class Activos_fijos
+class Activos_fijos
 {
-	//Implementamos nuestro constructor
-	public function __construct()
-	{
+  //Implementamos nuestro constructor
+  public function __construct()
+  {
+  }
 
-	}
-
-	//Implementamos un método para insertar registros
-	public function insertar($unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion,  $imagen)
-	{
-		// var_dump($unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion,  $imagen); die;
-		$sql = "INSERT INTO producto(idunidad_medida, idcolor, idcategoria_insumos_af, nombre, modelo, serie, marca, estado_igv, precio_unitario, precio_igv, precio_sin_igv, precio_total, ficha_tecnica, descripcion, imagen) 
+  //Implementamos un método para insertar registros
+  public function insertar($unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion, $imagen)
+  {
+    // var_dump($unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion,  $imagen); die;
+    $sql = "INSERT INTO producto(idunidad_medida, idcolor, idcategoria_insumos_af, nombre, modelo, serie, marca, estado_igv, precio_unitario, precio_igv, precio_sin_igv, precio_total, ficha_tecnica, descripcion, imagen) 
 		VALUES ('$unidad_medida', '$color', '$idcategoria', '$nombre', '$modelo', '$serie', '$marca', '$estado_igv', '$precio_unitario', '$precio_igv', '$precio_sin_igv', '$precio_total', '$ficha_tecnica', '$descripcion', '$imagen')";
-    	return ejecutarConsulta($sql);
-			
-	}
+    return ejecutarConsulta($sql);
+  }
 
-	//Implementamos un método para editar registros
-	public function editar($idproducto, $unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion,  $img_pefil)
-	{ 
-
-		$sql = "UPDATE producto SET 
+  //Implementamos un método para editar registros
+  public function editar($idproducto, $unidad_medida, $color, $idcategoria, $nombre, $modelo, $serie, $marca, $estado_igv, $precio_unitario, $precio_igv, $precio_sin_igv, $precio_total, $ficha_tecnica, $descripcion, $img_pefil)
+  {
+    $sql = "UPDATE producto SET 
 		idunidad_medida = '$unidad_medida',
 		idcolor = '$color',
 		idcategoria_insumos_af = '$idcategoria',
@@ -41,35 +38,62 @@ Class Activos_fijos
 		descripcion = '$descripcion',
 		imagen = '$img_pefil'
 		WHERE idproducto = '$idproducto'; ";
-		return ejecutarConsulta($sql);	
-	}
+    return ejecutarConsulta($sql);
+  }
 
-	//Implementamos un método para desactivar categorías
-	public function desactivar($idproducto )
-	{
-		$sql="UPDATE producto SET estado='0' WHERE idproducto ='$idproducto'";
-		return ejecutarConsulta($sql);
-	}
+  //Implementamos un método para desactivar categorías
+  public function desactivar($idproducto)
+  {
+    $sql = "UPDATE producto SET estado='0' WHERE idproducto ='$idproducto'";
+    return ejecutarConsulta($sql);
+  }
 
-	//Implementamos un método para activar categorías
-	public function activar($idproducto )
-	{
-		$sql="UPDATE producto SET estado='1' WHERE idproducto ='$idproducto'";
-		return ejecutarConsulta($sql);
-	}
+  //Implementamos un método para activar categorías
+  public function activar($idproducto)
+  {
+    $sql = "UPDATE producto SET estado='1' WHERE idproducto ='$idproducto'";
+    return ejecutarConsulta($sql);
+  }
 
-	//Implementar un método para mostrar los datos de un registro a modificar
-	public function mostrar($idproducto)
-	{
-		$sql="SELECT * FROM producto WHERE idproducto ='$idproducto'";
+  //Implementar un método para mostrar los datos de un registro a modificar
+  public function mostrar($idproducto)
+  {
+    $data = [];
 
-		return ejecutarConsultaSimpleFila($sql);
-	}
+    $sql = "SELECT idproducto, idunidad_medida, idcolor, idcategoria_insumos_af, nombre, 
+		modelo, serie, marca, estado_igv, precio_unitario, precio_igv, precio_sin_igv, 
+		precio_total, ficha_tecnica, descripcion, imagen, estado, fecha 
+		FROM producto WHERE idproducto ='$idproducto'";
+    $activos = ejecutarConsultaSimpleFila($sql);
 
-	//Implementar un método para listar los registros
-	public function listar()
-	{
-		$sql="SELECT
+    $data = [
+      'idproducto' => $activos['idproducto'],
+      'idunidad_medida' => $activos['idunidad_medida'],
+      'idcolor' => $activos['idcolor'],
+      'idcategoria_insumos_af' => $activos['idcategoria_insumos_af'],
+      'nombre' => decodeCadenaHtml($activos['nombre']),
+      'modelo' => decodeCadenaHtml($activos['modelo']),
+      'serie' => $activos['serie'],
+      'marca' => decodeCadenaHtml($activos['marca']),
+      'estado_igv' => $activos['estado_igv'],
+      'precio_unitario' => $activos['precio_unitario'],
+      'precio_igv' => $activos['precio_igv'],
+      'precio_sin_igv' => $activos['precio_sin_igv'],
+      'precio_total' => $activos['precio_total'],
+      'ficha_tecnica' => $activos['ficha_tecnica'],
+      'descripcion' => $activos['descripcion'],
+      'imagen' => $activos['imagen'],
+      'estado' => $activos['estado'],
+      'fecha' => $activos['fecha'],
+    ];
+
+    return $data;
+  }
+
+  //Implementar un método para listar los registros
+  public function listar()
+  {
+    $sql = "SELECT
 		p.idproducto AS idproducto,
 		p.idunidad_medida AS idunidad_medida,
 		p.idcolor AS idcolor,
@@ -90,39 +114,40 @@ Class Activos_fijos
 		FROM producto p, unidad_medida AS um, color AS c, categoria_insumos_af AS ciaf
 		WHERE um.idunidad_medida=p.idunidad_medida  AND c.idcolor=p.idcolor AND p.idcategoria_insumos_af != '1' AND ciaf.idcategoria_insumos_af = p.idcategoria_insumos_af
 		ORDER BY idproducto DESC";
-		return ejecutarConsulta($sql);		
-	}	
-	
+    return ejecutarConsulta($sql);
+  }
 
-	//Seleccionar Trabajador Select2
-	public function obtenerImg($idproducto)
-	{
-		$sql="SELECT imagen FROM producto WHERE idproducto='$idproducto'";
-		return ejecutarConsulta($sql);		
-	}
+  //Seleccionar Trabajador Select2
+  public function obtenerImg($idproducto)
+  {
+    $sql = "SELECT imagen FROM producto WHERE idproducto='$idproducto'";
+    return ejecutarConsulta($sql);
+  }
 
-	//Seleccionar una ficha tecnica
-	public function ficha_tec($idproducto)
-	{
-		$sql="SELECT ficha_tecnica FROM producto WHERE idproducto='$idproducto'";
-		return ejecutarConsulta($sql);		
-	}
+  //Seleccionar una ficha tecnica
+  public function ficha_tec($idproducto)
+  {
+    $sql = "SELECT ficha_tecnica FROM producto WHERE idproducto='$idproducto'";
+    return ejecutarConsulta($sql);
+  }
 
-	public function select2_color() {
-		$sql="SELECT idcolor AS id, nombre_color AS nombre FROM color WHERE estado='1' ORDER BY idcolor ASC;";
-		return ejecutarConsulta($sql);		
-	}
+  public function select2_color()
+  {
+    $sql = "SELECT idcolor AS id, nombre_color AS nombre FROM color WHERE estado='1' ORDER BY idcolor ASC;";
+    return ejecutarConsulta($sql);
+  }
 
-	public function select2_unidad_medida() {
-		$sql="SELECT idunidad_medida AS id, nombre_medida AS nombre, abreviacion FROM unidad_medida WHERE estado='1' ORDER BY nombre_medida ASC;";
-		return ejecutarConsulta($sql);		
-	}
+  public function select2_unidad_medida()
+  {
+    $sql = "SELECT idunidad_medida AS id, nombre_medida AS nombre, abreviacion FROM unidad_medida WHERE estado='1' ORDER BY nombre_medida ASC;";
+    return ejecutarConsulta($sql);
+  }
 
-	public function select2_categoria() {
-		$sql="SELECT idcategoria_insumos_af as id, nombre FROM categoria_insumos_af WHERE estado='1' AND idcategoria_insumos_af > 1 ORDER BY nombre ASC;";
-		return ejecutarConsulta($sql);		
-	}
-
+  public function select2_categoria()
+  {
+    $sql = "SELECT idcategoria_insumos_af as id, nombre FROM categoria_insumos_af WHERE estado='1' AND idcategoria_insumos_af > 1 ORDER BY nombre ASC;";
+    return ejecutarConsulta($sql);
+  }
 }
 
 ?>
