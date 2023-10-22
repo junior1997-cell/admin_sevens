b716a9cdaf980402b20382aacb2264f3e39103f7
davidrequejo <requejodavidmelvin@gmail.com>
2023-10-22 18:20:18 -0500

    15102317000

--- vistas/pago_impuesto.php
+++ vistas/pago_impuesto.php
@@ -60,6 +60,9 @@
                         <!-- /.card-header -->
                         <div class="card-body">
                           <div class="row">
+
+                            <!--VENTA-->
+
                             <div class="col-sm-12 col-md-9 col-lg-3 col-xl-3">
                               <table width="100%" class="table table-striped">
                                 <tr>
@@ -79,6 +82,9 @@
                                 </tr>
                               </table>
                             </div>
+
+                            <!--GASTOS CON FACTURA-->
+
                             <div class="col-sm-12 col-md-9 col-lg-3 col-xl-3">
                               <table width="100%" class="table table-striped">
                                 <tr>
@@ -111,6 +117,8 @@
                               </table>
                             </div>
 
+                            <!--UTILIDAD-->
+
                             <div class="col-sm-12 col-md-9 col-lg-2 col-xl-2">
                               <table  width="100%" class="table table-striped">
                                 <tr>
@@ -122,6 +130,8 @@
                                 </tr>
                               </table>
                             </div>
+
+                            <!--IMPUESTOS A PAGAR-->
 
                             <div class="col-sm-12 col-md-9 col-lg-2 col-xl-2">
                               <table width="100%" class="table table-striped">
@@ -142,6 +152,9 @@
                                 </tr>
                               </table>
                             </div>
+
+                            <!--UTILIDAD NETA-->
+
                             <div class="col-sm-12 col-md-9 col-lg-2 col-xl-2">
                               <table width="100%" class="table table-striped">
                                 <tr>
@@ -168,213 +181,6 @@
                 </div>
                 <!-- /.container-fluid -->
 
-                <!-- Modal agregar Recibo por Honorario -->
-                <div class="modal fade" id="modal-agregar-recibo">
-                  <div class="modal-dialog modal-dialog-scrollable modal-lg">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h4 class="modal-title"><b>Agregar:</b>Pago de impuestos</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span class="text-danger" aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-
-                      <div class="modal-body">
-                        <!-- form start -->
-                        <form id="form-recibo" name="form-recibo" method="POST">
-                          <div class="card-body">
-                            <div class="row" id="cargando-1-fomulario">
-                              <!-- id proyecto -->
-                              <input type="hidden" name="idproyecto" id="idproyecto" />
-                              <!-- id hospedaje -->
-                              <input type="hidden" name="idrecibo_x_honorario" id="idrecibo_x_honorario" />
-
-                              <!-- Tipo de documento -->
-                              <div class="col-12 col-sm-6 col-md-6 col-lg-2">
-                                <div class="form-group">
-                                  <label for="tipo_documento">Tipo de doc.</label>
-                                  <select name="tipo_documento" id="tipo_documento" class="form-control" placeholder="Tipo de doc.">
-                                    <option selected value="DNI">DNI</option>
-                                  </select>
-                                </div>
-                              </div>
-
-                              <!-- N° de documento -->
-                              <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-                                <div class="form-group">
-                                  <label for="num_documento">N° de doc. <sup class="text-danger">(unico*)</sup></label>
-                                  <div class="input-group">
-                                    <input type="number" name="num_documento" class="form-control" id="num_documento" placeholder="N° de documento" />
-                                    <div class="input-group-append" data-toggle="tooltip" data-original-title="Buscar Reniec/SUNAT" onclick="buscar_sunat_reniec();">
-                                      <span class="input-group-text" style="cursor: pointer;">
-                                        <i class="fas fa-search text-primary" id="search"></i>
-                                        <i class="fa fa-spinner fa-pulse fa-fw fa-lg text-primary" id="charge" style="display: none;"></i>
-                                      </span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <!-- Nombre -->
-                              <div class="col-12 col-sm-12 col-md-12 col-lg-6">
-                                <div class="form-group">
-                                  <label for="nombre">Nombre y Apellidos <sup class="text-danger">*</sup></label>
-                                  <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Nombres y apellidos" />
-                                </div>
-                              </div>
-                              <!-- Fecha 1  -->
-                              <div class="col-lg-4 class_pading">
-                                <div class="form-group">
-                                  <label for="fecha_pago">Fecha Emisión  <sup class="text-danger">*</sup></label>
-                                  <input type="date" name="fecha_pago" class="form-control" id="fecha_pago" />
-                                </div>
-                              </div>
-
-                              <!--Precio Parcial-->
-                              <div class="col-lg-4 class_pading">
-                                <div class="form-group">
-                                  <label for="monto">Monto <sup class="text-danger">*</sup></label>
-                                  <input type="number" name="monto" id="monto" class="form-control" placeholder="Monto" />                                  
-                                </div>
-                              </div>
-                              <!-- Costo -->
-                              <div class="col-lg-4">
-                                <div class="form-group">
-                                  <label for="costo">Costo <sup class="text-danger">*</sup></label>
-                                  <input class="form-control" type="number" id="costo" name="costo" placeholder="Costo" />                               
-                                </div>
-                              </div>
-
-                              <!--servicio-->
-                              <div class="col-lg-12 class_pading">
-                                <div class="form-group">
-                                  <label for="servicio">Servicio <sup class="text-danger">*</sup></label> <br />
-                                  <textarea name="servicio" id="servicio" class="form-control" rows="2"></textarea>
-                                </div>
-                              </div>
-
-                              <!-- RECIBO -->
-                              <div class="col-md-6" >                               
-                                <div class="row text-center">
-                                  <div class="col-md-12" style="padding-top: 15px; padding-bottom: 5px;">
-                                    <label for="cip" class="control-label" > Recibo </label>
-                                  </div>
-                                  <div class="col-6 col-md-6 text-center">
-                                    <button type="button" class="btn btn-success btn-block btn-xs" id="doc1_i"><i class="fas fa-upload"></i> Subir.</button>
-                                    <input type="hidden" id="doc_old_1" name="doc_old_1" />
-                                    <input style="display: none;" id="doc1" type="file" name="doc1" accept="application/pdf, image/*" class="docpdf" /> 
-                                  </div>
-                                  <div class="col-6 col-md-6 text-center">
-                                    <button type="button" class="btn btn-info btn-block btn-xs" onclick="re_visualizacion(1, 'compra_rh','recibo');">
-                                    <i class="fas fa-redo"></i> Recargar.
-                                    </button>
-                                  </div>
-                                </div>                              
-                                <div id="doc1_ver" class="text-center mt-4">
-                                  <img src="../dist/svg/doc_uploads.svg" alt="" width="50%" >
-                                </div>
-                                <div class="text-center" id="doc1_nombre"><!-- aqui va el nombre del pdf --></div>
-                              </div>
-
-                              <!-- VOUCHER -->
-                              <div class="col-md-6" >                               
-                                <div class="row text-center">
-                                  <div class="col-md-12" style="padding-top: 15px; padding-bottom: 5px;">
-                                    <label for="cip" class="control-label" > Voucher de depósito </label>
-                                  </div>
-                                  <div class="col-6 col-md-6 text-center">
-                                    <button type="button" class="btn btn-success btn-block btn-xs" id="doc2_i"><i class="fas fa-upload"></i> Subir.</button>
-                                    <input type="hidden" id="doc_old_2" name="doc_old_2" />
-                                    <input style="display: none;" id="doc2" type="file" name="doc2" accept="application/pdf, image/*" class="docpdf" /> 
-                                  </div>
-                                  <div class="col-6 col-md-6 text-center">
-                                    <button type="button" class="btn btn-info btn-block btn-xs" onclick="re_visualizacion(2, 'compra_rh','voucher');">
-                                    <i class="fas fa-redo"></i> Recargar.
-                                    </button>
-                                  </div>
-                                </div>                              
-                                <div id="doc2_ver" class="text-center mt-4">
-                                  <img src="../dist/svg/doc_uploads.svg" alt="" width="50%" >
-                                </div>
-                                <div class="text-center" id="doc2_nombre"><!-- aqui va el nombre del pdf --></div>
-                              </div>
-
-                              <!-- barprogress -->
-                              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:20px;">
-                                <div class="progress" id="barra_progress_recibo_div">
-                                  <div id="barra_progress_recibo" class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 0%;">
-                                    0%
-                                  </div>
-                                </div>
-                              </div>
-
-                            </div>
-
-                            <div class="row" id="cargando-2-fomulario" style="display: none;">
-                              <div class="col-lg-12 text-center">
-                                <i class="fas fa-spinner fa-pulse fa-6x"></i><br />
-                                <br />
-                                <h4>Cargando...</h4>
-                              </div>
-                            </div>
-                          </div>
-                          <!-- /.card-body -->
-                          <button type="submit" style="display: none;" id="submit-form-recibo">Submit</button>
-                        </form>
-                      </div>
-                      <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="limpiar();">Close</button>
-                        <button type="submit" class="btn btn-success" id="guardar_registro">Guardar Cambios</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <!--===============Modal-ver-comprobante =========-->
-                <div class="modal fade" id="modal-ver-comprobante">
-                  <div class="modal-dialog modal-dialog-scrollable modal-lg">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h4 class="modal-title nombre_comprobante"></h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span class="text-danger" aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                          <div class="row">
-                            <div class="col-6 col-md-6">
-                              <a class="btn btn-xs btn-block btn-warning" href="#" id="iddescargar" download="" type="button"><i class="fas fa-download"></i> Descargar</a>
-                            </div>
-                            <div class="col-6 col-md-6">
-                              <a class="btn btn-xs btn-block btn-info" href="#" id="ver_completo"  target="_blank" type="button"><i class="fas fa-expand"></i> Ver completo.</a>
-                            </div>
-                            <div class="col-12 col-md-12 mt-2">
-                              <div id="ver_fact_pdf" width="auto"></div>
-                            </div>
-                          </div>                          
-                        </div>
-                    </div>
-                  </div>
-                </div>
-
-                <!--Modal ver datos-->
-                <div class="modal fade" id="modal-ver-recibo">
-                  <div class="modal-dialog modal-dialog-scrollable modal-xm">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h4 class="modal-title">Detalles</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span class="text-danger" aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-
-                      <div class="modal-body">
-                        <div id="datosrecibo" class="class-style">
-                          <!-- vemos los datos del trabajador -->
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
               </section>
               <!-- /.content -->
             </div>
