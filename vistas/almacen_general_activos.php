1a66a122cdf98ccd4a1ffa59b6f4a1cb66ce4ef2
davidrequejo <requejodavidmelvin@gmail.com>
2024-11-24 23:58:59 -0300

    cambios 24112024_1

--- vistas/almacen_general_activos.php
+++ vistas/almacen_general_activos.php
@@ -647,7 +647,7 @@ if (!isset($_SESSION["nombre"])) {
     <!-- /.content-wrapper -->
     <?php require 'script.php'; ?>
 
-    <script type="text/javascript" src="scripts/almacen_general_activos.js?version_jdl=1.6"></script>
+    <script type="text/javascript" src="scripts/almacen_general_activos.js?version_jdl=1.7"></script>
 
     <script>
       $(function() {
